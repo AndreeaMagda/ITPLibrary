2ab71b25227e8153eaedbc1b1efdfefbea44df28
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-08-10 20:18:34 +0300

    Project setup

--- ITPLibrary.Api.Data/obj/Debug/net8.0/ITPLibrary.Api.Data.AssemblyInfo.cs
+++ ITPLibrary.Api.Data/obj/Debug/net8.0/ITPLibrary.Api.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITPLibrary.Api.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7db245b666c3a608e66bf29296e20fdf628af7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+042cab5c286c4f861004a480b7520a8132b2216d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITPLibrary.Api.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITPLibrary.Api.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
